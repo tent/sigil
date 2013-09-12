8a8b4220f3473d62fab94459ea775fa7d9b63dff
Jonathan Rudenberg <jonathan@titanous.com>
2013-09-12 00:13:45 -0400

    Add benchmarks

---
+++ gen/gen_test.go
@@ -0,0 +1,42 @@
+package gen
+
+import (
+	"encoding/hex"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"testing"
+)
+
+var data, _ = hex.DecodeString("0447550ed53b522827e17b7d7976dd3f")
+var config = Sigil{
+	Width:   420,
+	Columns: 3,
+	Rows:    5,
+	Foreground: []color.Color{
+		rgb(45, 79, 255),
+		rgb(44, 172, 0),
+		rgb(254, 180, 44),
+		rgb(226, 121, 234),
+		rgb(30, 179, 253),
+		rgb(232, 77, 65),
+		rgb(49, 203, 115),
+		rgb(141, 69, 170),
+		rgb(252, 125, 31),
+	},
+	Background: rgb(224, 224, 224),
+}
+
+func rgb(r, g, b uint8) color.NRGBA { return color.NRGBA{r, g, b, 255} }
+
+func BenchmarkMake(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		config.Make(data)
+	}
+}
+
+func BenchmarkMakeEncode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		png.Encode(ioutil.Discard, config.Make(data))
+	}
+}
